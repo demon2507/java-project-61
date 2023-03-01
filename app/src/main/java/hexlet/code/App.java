42084cbb7e7c620ac177a405010b44e58278de57
dima Minakov <vano25071993@yandex.ru>
2023-03-01 19:08:59 +0700

    making list of games for game start

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,8 +1,15 @@
 package hexlet.code;
-
+import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Brain Games!");
+        Scanner  choice = new Scanner(System.in);
+        System.out.println("Please enter the game number and press Enter.");
+        System.out.println("1 - Greet");
+        System.out.println("2 - Even");
+        System.out.println("0 - Exit");
+        String numberOfGame = choice.next();
+        System.out.println("Your choice: " + numberOfGame);
+        System.out.print("Welcome to the Brain Games!");
         Cli.askingNameUser();
     }
 }
