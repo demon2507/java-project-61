ce4550cbcf85cd24521c390a1c7391a3ba08ffb6
dima Minakov <vano25071993@yandex.ru>
2023-03-07 20:58:36 +0700

    improve IsEven class

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -7,8 +7,9 @@ public class App {
         System.out.println("1 - Greet");
         System.out.println("2 - Even");
         System.out.println("0 - Exit");
+        System.out.print("Your choice: ");
         String numberOfGame = choice.next();
-        System.out.println("Your choice: " + numberOfGame);
+        System.out.print("\n");
         switch(numberOfGame) {
             case "0":
                 System.exit(0);
@@ -19,8 +20,8 @@ public class App {
                 break;
             case "2":
                 System.out.println("Welcome to the Brain Games!");
-                Cli.askingNameUser();
                 IsEven.checkNumber();
+            break;
         }
     }
 }
--- app/src/main/java/hexlet/code/IsEven.java
+++ app/src/main/java/hexlet/code/IsEven.java
@@ -3,46 +3,28 @@ import java.util.Scanner;
 
 public class IsEven {
     public static void checkNumber() {
+        System.out.print("May I have your name? ");
+        Scanner  user = new Scanner(System.in);
+        String userName = user.next();
+        System.out.println("Hello, " + userName + "!");
         String correctAnswer = "Correct!";
         int firstNumber = 0; // Начальное значение диапазона - "от"
         int lastNumber = 100; // Конечное значение диапазона - "до"
-
-        int firstRandomNumber = firstNumber + (int) (Math.random() * lastNumber);
-        int secondRandomNumber = firstNumber + (int) (Math.random() * lastNumber);
-        int thirdRandomNumber = firstNumber + (int) (Math.random() * lastNumber);
-        String isEvenFirstNum = firstRandomNumber % 2 == 0 ? "yes" : "no";
-        String isEvenSecondNum = secondRandomNumber % 2 == 0 ? "yes" : "no";
-        String isEvenThirdNum = thirdRandomNumber % 2 == 0 ? "yes" : "no";
         System.out.println("Answer 'yes' if the number is even, otherwise answer 'no'.");
-        Scanner  userAnswer = new Scanner(System.in);
-        System.out.print("Question: " + firstRandomNumber + "\n");
-        String firstAnswer = userAnswer.next();
-        System.out.println("Your answer: " + firstAnswer);
-        if(firstAnswer.equals(isEvenFirstNum)) {
-            System.out.println(correctAnswer);
-        }
-            else{
-            System.out.println("'" + firstAnswer +  "'" + " is wrong answer ;(. Correct answer was " + "'" + isEvenFirstNum + "'");
-            }
-        System.out.print("Question: " + secondRandomNumber + "\n");
-        String secondAnswer = userAnswer.next();
-        System.out.println("Your answer: " + secondAnswer);
-        if(secondAnswer.equals(isEvenSecondNum)) {
-            System.out.println(correctAnswer);
-        }
-            else{
-            System.out.println("'" + secondAnswer +  "'" + " is wrong answer ;(. Correct answer was " + "'" + isEvenSecondNum+ "'");
-            }
-        System.out.print("Question: " + thirdRandomNumber + "\n");
-        String thirdAnswer = userAnswer.next();
-        System.out.println("Your answer: " + thirdAnswer);
-        if(thirdAnswer.equals(isEvenThirdNum)) {
-            System.out.println(correctAnswer);
-        }
-        else{
-            System.out.println("'" + thirdAnswer +  "'" + " is wrong answer ;(. Correct answer was " + "'" + isEvenThirdNum+ "'");
-        }
+            for(int i = 0; i < 3; i++) {
+            int randomNum = firstNumber + (int) (Math.random() * lastNumber);
+            String isEvenRandomNum = randomNum % 2 == 0 ? "yes" : "no";
+            System.out.print("Question: " + randomNum + "\n");
+            System.out.print("Your answer: ");
+            String answer = user.next();
+            if(answer.equals(isEvenRandomNum)) {
+                System.out.println(correctAnswer);
+                } else { System.out.println("'" + answer +  "'" + " is wrong answer ;(. Correct answer was " + "'" + isEvenRandomNum + "'" + "\n" + "Let`s try again, " + userName + "!");
+                    System.exit(0);
 
+                }
+            }
+         System.out.print("Congratulations, " + userName + "!");
 
     }
 }
