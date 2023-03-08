b11887df37336a8290aeba3ff49c4664e250e224
dima Minakov <vano25071993@yandex.ru>
2023-03-08 19:24:17 +0700

    adding choice of Calculator game

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -6,6 +6,7 @@ public class App {
         System.out.println("Please enter the game number and press Enter.");
         System.out.println("1 - Greet");
         System.out.println("2 - Even");
+        System.out.println("3 - Calc");
         System.out.println("0 - Exit");
         System.out.print("Your choice: ");
         String numberOfGame = choice.next();
@@ -22,6 +23,9 @@ public class App {
                 System.out.println("Welcome to the Brain Games!");
                 IsEven.checkNumber();
             break;
+            case "3":
+                Calc.calculator();
+                break;
         }
     }
 }
